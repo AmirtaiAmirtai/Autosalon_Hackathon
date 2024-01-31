3a07b3cd3de897a50a7679f9f0fcdd6db0e7e57d
AmirtaiAmirtai <amirtay.omurov@gmail.com>
2024-01-31 13:54:12 +0600

    Добавьте файлы проекта.

---
+++ харкатон/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace харкатон
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ... другие настройки ...
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            // ... другие настройки ...
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Toy Store API V1");
+            });
+        }
+    }
+}
---
+++ харкатон/Новая папка/products.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace харкатон.Controllers.models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
